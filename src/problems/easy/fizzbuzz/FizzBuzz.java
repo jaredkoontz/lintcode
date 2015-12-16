bb96197bb314c7d93fe2d5e2ba603828ecc5fb38
koontz <jkoontz09@gmail.com>
2015-12-16 11:19:02 -0700

    more solutions

--- src/problems/easy/fizzbuzz/FizzBuzz.java
+++ src/problems/easy/fizzbuzz/FizzBuzz.java
@@ -1,7 +1,25 @@
 package problems.easy.fizzbuzz;
 
-/**
- * Created by jkoontz6721 on 12/14/15.
- */
+import java.util.ArrayList;
+
 public class FizzBuzz {
+	/**
+	 * param n: As description.
+	 * return: A list of strings.
+	 */
+	public ArrayList<String> fizzBuzz(int n) {
+		ArrayList<String> results = new ArrayList<>();
+		for (int i = 1; i <= n; i++) {
+			if (i % 15 == 0) {
+				results.add("fizz buzz");
+			} else if (i % 5 == 0) {
+				results.add("buzz");
+			} else if (i % 3 == 0) {
+				results.add("fizz");
+			} else {
+				results.add(String.valueOf(i));
+			}
+		}
+		return results;
+	}
 }
