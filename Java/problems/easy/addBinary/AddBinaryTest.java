f892aa47cf6a845feb85862d1d2a9fbfda6d8091
koontz <jkoontz09@gmail.com>
2016-01-28 10:29:50 -0700

    add binary

---
+++ Java/problems/easy/addBinary/AddBinaryTest.java
@@ -0,0 +1,20 @@
+package problems.easy.addBinary;
+
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
+/**
+ * @author koontz
+ */
+public class AddBinaryTest {
+
+	@Test
+	public void happyPath() {
+		String a = "11";
+		String b = "1";
+		AddBinary addBinary = new AddBinary();
+		assertEquals(addBinary.addBinary(a, b), "100");
+	}
+
+}
