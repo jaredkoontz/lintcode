38073bd2fe4645cb39e35d4b23b05ffbf6032f74
koontz <jkoontz09@gmail.com>
2016-01-31 20:33:14 -0700

    happy number

---
+++ Java/problems/easy/happyNumber/HappyNumberTest.java
@@ -0,0 +1,17 @@
+package problems.easy.happyNumber;
+
+import org.junit.Test;
+
+/**
+ * @author koontz
+ */
+public class HappyNumberTest {
+
+	@Test
+	public void happyPath(){
+		int happyNum = 19;
+		HappyNumber happyNumber = new HappyNumber();
+		System.out.println(happyNumber.isHappy(happyNum));
+	}
+
+}
